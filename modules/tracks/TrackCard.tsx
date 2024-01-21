feed562ad9a310eb2ad6401d1ee2fe352158fd6c
gregogun <gregogun97@gmail.com>
2024-01-21 21:25:26 +0000

    feat: add dropdown

--- modules/tracks/TrackCard.tsx
+++ modules/tracks/TrackCard.tsx
@@ -1,11 +1,33 @@
 import { css } from "@/styles/css";
 import { css as stitchesCss } from "@stitches/react";
 import { DotsHorizontalIcon, HeartIcon, PlayIcon } from "@radix-ui/react-icons";
-import { AspectRatio, Box, Flex, IconButton, Text } from "@radix-ui/themes";
-import { MdPause, MdPlayArrow } from "react-icons/md";
+import {
+  AspectRatio,
+  Box,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+  Flex,
+  IconButton,
+  Text,
+} from "@radix-ui/themes";
+import {
+  MdLink,
+  MdPause,
+  MdPlayArrow,
+  MdPlaylistAdd,
+  MdPlaylistPlay,
+  MdShare,
+} from "react-icons/md";
 import { IoMdHeart } from "react-icons/io";
 import { styled } from "@stitches/react";
 import { useState } from "react";
+
+const StyledDropdownMenuItem = styled(DropdownMenuItem, {
+  justifyContent: "start",
+  gap: "var(--space-2)",
+});
 
 const ActionsOverlay = styled(Flex, {
   width: "100%",
@@ -13,31 +35,18 @@ const ActionsOverlay = styled(Flex, {
   position: "absolute",
   padding: "var(--space-3)",
   opacity: 0,
+  background: `linear-gradient(
+    to top,
+    var(--black-a12) 0%,
+    var(--black-a3) 50%,
+    var(--black-a2) 65%,
+    var(--black-a1) 75.5%,
+    var(--black-a1) 82.85%,
+    var(--black-a1) 88%,
+    var(--black-a1) 100%
+      )`,
 
-  "&:hover": {
-    background: `linear-gradient(
-      to top,
-      var(--black-a12) 0%,
-      var(--black-a3) 50%,
-      var(--black-a2) 65%,
-      var(--black-a1) 75.5%,
-      var(--black-a1) 82.85%,
-      var(--black-a1) 88%,
-      var(--black-a1) 100%
-        )`,
-    opacity: 1,
-  },
-  "&:has(:focus-visible)": {
-    background: `linear-gradient(
-      to top,
-      var(--black-a12) 0%,
-      var(--black-a3) 50%,
-      var(--black-a2) 65%,
-      var(--black-a1) 75.5%,
-      var(--black-a1) 82.85%,
-      var(--black-a1) 88%,
-      var(--black-a1) 100%
-        )`,
+  "&:hover, &:has(:focus-visible)": {
     opacity: 1,
   },
 });
@@ -48,6 +57,17 @@ const AlphaIconButton = styled(IconButton, {
 
   "&:hover": {
     color: "var(--white-a12)",
+  },
+
+  variants: {
+    liked: {
+      true: {
+        color: "var(--red-9)",
+        "&:hover": {
+          color: "var(--red-10)",
+        },
+      },
+    },
   },
 });
 
@@ -68,26 +88,9 @@ const AlphaPlayIconButton = styled(IconButton, {
 const TRACK_ITEM_SIZE = 220;
 const OUTLINE_OFFSET = 0.5;
 
-const childFocused = stitchesCss({
-  "& button": {
-    "&:focus": {
-      background: `linear-gradient(
-        to top,
-        var(--black-a12) 0%,
-        var(--black-a3) 50%,
-        var(--black-a2) 65%,
-        var(--black-a1) 75.5%,
-        var(--black-a1) 82.85%,
-        var(--black-a1) 88%,
-        var(--black-a1) 100%
-          )`,
-      opacity: 1,
-    },
-  },
-});
-
 export const TrackCard = () => {
   const [playing, setPlaying] = useState(false);
+  const [liked, setLiked] = useState(false);
 
   return (
     <Box asChild>
@@ -99,18 +102,6 @@ export const TrackCard = () => {
             position: "relative",
           })}
         >
-          {/* <AspectRatio
-        className="trackArt"
-        ratio={1 / 1}
-        style={css({
-          objectFit: "cover",
-          width: `calc(${TRACK_ITEM_SIZE}px * var(--scaling))`,
-          height: `calc(${TRACK_ITEM_SIZE}px * var(--scaling))`,
-          outline: `${OUTLINE_OFFSET}px solid var(--white-a3)`,
-          outlineOffset: -OUTLINE_OFFSET,
-          position: "relative",
-        })}
-      > */}
           <Box
             style={css({
               width: `calc(${TRACK_ITEM_SIZE}px * var(--scaling))`,
@@ -120,46 +111,49 @@ export const TrackCard = () => {
               position: "relative",
             })}
           >
-            <ActionsOverlay
-              justify="between"
-              align="end"
-              gap="3"
-              // style={css({
-              //   width: "100%",
-              //   height: "100%",
-              //   position: "absolute",
-              //   padding: "var(--space-3)",
-              //   opacity: 0,
-
-              //   "&:showOverlay": {
-              //     background: `linear-gradient(
-              //     to top,
-              //     var(--black-a12) 0%,
-              //     var(--black-a3) 50%,
-              //     var(--black-a2) 65%,
-              //     var(--black-a1) 75.5%,
-              //     var(--black-a1) 82.85%,
-              //     var(--black-a1) 88%,
-              //     var(--black-a1) 100%
-              //       )`,
-              //     opacity: 1,
-              //   },
-              // })}
-            >
+            <ActionsOverlay justify="between" align="end" gap="3">
               <AlphaPlayIconButton
                 onClick={() => setPlaying(!playing)}
                 variant="soft"
-                size="4"
+                size="3"
               >
                 {playing ? <MdPause /> : <MdPlayArrow />}
               </AlphaPlayIconButton>
               <Flex align="center" gap="3">
-                <AlphaIconButton size="2" variant="ghost" highContrast>
+                <AlphaIconButton
+                  onClick={() => setLiked(!liked)}
+                  liked={liked}
+                  size="2"
+                  variant="ghost"
+                  highContrast
+                >
                   <IoMdHeart width={15} height={15} />
                 </AlphaIconButton>
-                <AlphaIconButton size="2" variant="ghost" highContrast>
-                  <DotsHorizontalIcon />
-                </AlphaIconButton>
+                <DropdownMenuRoot>
+                  <DropdownMenuTrigger>
+                    <AlphaIconButton size="2" variant="ghost" highContrast>
+                      <DotsHorizontalIcon />
+                    </AlphaIconButton>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent data-track-actions-dropdown>
+                    <StyledDropdownMenuItem>
+                      <MdShare />
+                      Share
+                    </StyledDropdownMenuItem>
+                    <StyledDropdownMenuItem>
+                      <MdLink />
+                      Copy link
+                    </StyledDropdownMenuItem>
+                    <StyledDropdownMenuItem>
+                      <MdPlaylistAdd />
+                      Add to playlist
+                    </StyledDropdownMenuItem>
+                    <StyledDropdownMenuItem>
+                      <MdPlaylistPlay />
+                      Add to queue
+                    </StyledDropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenuRoot>
               </Flex>
             </ActionsOverlay>
             <img
@@ -171,7 +165,6 @@ export const TrackCard = () => {
                 height: "100%",
               })}
             />
-            {/* </AspectRatio> */}
           </Box>
           <Flex direction="column">
             <Text size="1">Track Title</Text>
