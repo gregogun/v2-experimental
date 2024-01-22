4c40ed003ff99184929d3b9d960eaf4a1299476c
gregogun <gregogun97@gmail.com>
2024-01-22 00:51:23 +0000

    feat: add track item with placeholder functionality

--- modules/tracks/TrackGrid.tsx
+++ modules/tracks/TrackGrid.tsx
@@ -1,11 +1,15 @@
-import { Grid } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { TrackCard } from "./TrackCard";
+import { TrackItem } from "./TrackItem";
 
 export const TrackGrid = () => {
   return (
-    <Grid p="5" asChild>
+    <Grid p="5" gap="9" asChild>
       <ul>
         <TrackCard />
+        <Box style={{ maxWidth: 500 }}>
+          <TrackItem />
+        </Box>
       </ul>
     </Grid>
   );
